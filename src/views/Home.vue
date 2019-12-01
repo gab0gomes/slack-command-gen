3f122e37a0b00b79d9ddddbbbdf2fcee2eb7c2b4
Gabriel Rafael <gabrielrafael2508@hotmail.com>
2019-12-01 18:14:49 -0300

    Inicia implementação da home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,113 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+      <section class="hero is-medium is-primary is-bold">
+        <div class="hero-body">
+          <div class="container">
+            <h1 class="title">
+              Slack Command Generator
+            </h1>
+            <h2 class="subtitle">
+              A simple native and plugins-based command generator to Slack
+            </h2>
+          </div>
+        </div>
+    </section>
+    <section class="section">
+      <div class="container">
+        <div class="columns">
+          <div class="column is-one-quarter">
+            <b-field
+              label="Plugin"
+              label-position="on-border"
+            >
+              <b-select
+                v-model="selectedPlugin"
+                placeholder="Select a plugin"
+                expanded
+              >
+                  <option
+                      v-for="plugin in plugins"
+                      :value="plugin"
+                      :key="plugin">
+                      {{ plugin }}
+                  </option>
+              </b-select>
+            </b-field>
+            </div>
+          <div class="column is-one-quarter">
+            <b-field
+              label="Action"
+              label-position="on-border"
+            >
+                <b-select
+                  v-model="selectedAction"
+                  :placeholder="(!selectedPlugin ? 'Select a plugin first' : 'Select an action')"
+                  :disabled="(!selectedPlugin)"
+                  expanded
+                >
+                    <option
+                        v-for="action in actions"
+                        :value="action"
+                        :key="action">
+                        {{ action }}
+                    </option>
+                </b-select>
+            </b-field>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-content">
+            <div class="content">
+              <form-generator
+                v-if="!_.isEmpty(fields)"
+                :fields="fields"
+              />
+              <b-message
+                v-else
+              >
+                No action selected
+              </b-message>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import commands from '../constants/commands';
+import FormGenerator from '../components/formGenerator.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    FormGenerator,
+  },
+
+  data() {
+    return {
+      selectedAction: null,
+      selectedPlugin: null,
+    };
+  },
+
+  computed: {
+    plugins() {
+      return this._.keys(commands);
+    },
+
+    actions() {
+      return !this._.isEmpty(this.selectedPlugin)
+        ? this._.keys(commands[this.selectedPlugin].commands)
+        : [];
+    },
+
+    fields() {
+      return !this._.isEmpty(this.selectedAction)
+        ? commands[this.selectedPlugin].commands[this.selectedAction].params
+        : null;
+    },
   },
 };
 </script>
