346170e171a55f1ced50891d4ef1352bb8c471e0
Gabriel Rafael <gabrielrafael2508@hotmail.com>
2019-12-01 18:13:13 -0300

    Adiciona buefy e lodash ao projeto

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue';
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
+import _ from 'lodash';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+Object.defineProperty(Vue.prototype, '_', { value: _ });
+
+Vue.use(Buefy);
 
 Vue.config.productionTip = false;
 
