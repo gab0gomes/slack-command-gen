2f7d8d3c83a7ce031e8f0e12cdc26b6993f7d7a6
Gabriel Rafael <gabrielrafael2508@hotmail.com>
2019-12-01 18:13:49 -0300

    Cria gerador de form

---
+++ src/components/formGenerator.vue
@@ -0,0 +1,41 @@
+<template>
+  <form>
+    <b-field
+      v-for="(field, key) in fields"
+      :key="key"
+      :label="_.capitalize(key)"
+      label-position="on-border"
+    >
+      <component :is="component(field.type)"/>
+    </b-field>
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    fields: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  methods: {
+    component(type) {
+      switch (type) {
+        case 'text':
+          return 'b-input';
+        case 'select':
+          return 'b-select';
+        case 'date':
+          return 'b-datetimepicker';
+        default:
+          return 'b-input';
+      }
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
